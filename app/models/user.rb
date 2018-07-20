2952a0363d3136cac19f148e311eb30da022d022
Loserbirdman5566 <kobe24684@gmail.com>
2018-07-20 11:26:53 +0800

    set up admin authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  def admin?
+    self.role == "admin"
+  end
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
 end
