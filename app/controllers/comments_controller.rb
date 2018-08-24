55a54ba35581513df97d210fd93ea6af84594faa
Loserbirdman5566 <kobe24684@gmail.com>
2018-08-24 11:35:14 +0800

    Users of this comment can delete the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,15 @@ class CommentsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = @restaurant.comments.build(comment_params)
     @comment.user = current_user
-    @comment.save!
+    @comment.save! # 與 @comment.save 作比較
     redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:id]) # @comment = @restaurant.comments.find(params[:id])
 
-    if current_user.admin?
+    if current_user.admin? || current_user == @comment.user
       @comment.destroy
       redirect_to restaurant_path(@restaurant)
     end
