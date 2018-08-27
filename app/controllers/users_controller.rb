aa293ea118081dff238a13d07762b69e954069b0
Loserbirdman5566 <kobe24684@gmail.com>
2018-08-27 12:14:18 +0800

    user can update the profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
+    
+  end
+
+  # !(如果使用者 == 現在的使用者)  跳回編輯頁面 
+  def edit  
+    unless @user == current_user  
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
+
+  def user_params
+    params.require(:user).permit(:name, :intro)    
+  end  
 
 end
